46223384ab668c9bdcc9304efbbb9f60aeca9df4
Romain Gauthier <romain.gauthier@monkeypatch.me>
2011-12-14 22:19:44 +0100

    Implements the basic sync features.
    Implements the support of CRUD methods, but still lacks of emulateHTTP and
    emulateTNetStings. However, should be usable yet.

---
+++ backbone-tnetstrings.js
@@ -0,0 +1,51 @@
+(function(){
+
+    // Map from CRUD to HTTP for our default `Backbone.sync` implementation.
+    var methodMap = {
+        'create': 'POST',
+        'update': 'PUT',
+        'delete': 'DELETE',
+        'read'  : 'GET'
+    };
+
+    // Backbone.TNetStrings.sync
+    // -------------
+
+    //
+    Backbone.TNetStrings = {
+        sync: function(method, model, options, error) {
+            var type = methodMap[method];
+
+            // Default TNetStrings-request options.
+            var params = {type : type, dataType : 'tnetstring'};
+
+            // Ensure that we have a URL.
+            if (!options.url) {
+                params.url = getUrl(model) || urlError();
+            }
+
+            // Ensure that we have the appropriate request data.
+            if (!options.data && model && (method == 'create' || method == 'update')) {
+                params.contentType = 'application/tnetstring';
+                params.data = tnetstrings.dump(model.toJSON());
+            }
+
+            // Make the request, allowing the user to override any Ajax options.
+            return $.ajax(_.extend(params, options));
+        }
+    };
+
+    // Helper function to get a URL from a Model or Collection as a property
+    // or as a function.
+    var getUrl = function(object) {
+        if (!(object && object.url)) return null;
+        return _.isFunction(object.url) ? object.url() : object.url;
+    };
+
+    // Throw an error when a URL is needed, and none is supplied.
+    var urlError = function() {
+        throw new Error('A "url" property or function must be specified');
+    };
+
+}).call(this);
+
